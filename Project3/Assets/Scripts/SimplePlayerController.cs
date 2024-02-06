f0234cda977879b1a31a008ffc903e4a37f2fa13
Andrew-Panzone <panzone.an@northeastern.edu>
2024-02-06 14:16:29 -0500

    cube mechanics testing

---
+++ Project3/Assets/Scripts/SimplePlayerController.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SimplePlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        transform.position += movement * speed * Time.deltaTime;
+    }
+}
