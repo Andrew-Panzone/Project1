a47da2b763de5601d2dde5aec5969905be686627
me1111o <yang.ruix@northeastern.edu>
2024-02-08 13:47:27 -0500

    Scene

---
+++ Project2/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,80 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void CameraController_Start_m8EAAC188934ECCBD009EC00751280DCFF4D96C71 (void);
+extern void CameraController_Update_mBCB871B23DBA60444D43AB56D780478BA3D355E6 (void);
+extern void CameraController__ctor_mE196A6332BDDED632D6F9DB6260E424594598950 (void);
+extern void FlickeringLight_Start_m7D516566C903A2F68F7F1471820D1A9A029AB3A1 (void);
+extern void FlickeringLight_Update_m155AEC6E38625104EEF180A44414F6FD7F46264F (void);
+extern void FlickeringLight__ctor_m05A8934D0DC3CC94EC5F4C71CEFFEE2F749759EF (void);
+extern void PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF (void);
+extern void PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33 (void);
+extern void PlayerController_OnTriggerEnter_mF2704872AEA62333341DCBEA2831320C385701D1 (void);
+extern void PlayerController_OnTriggerExit_m0A2DB7CCC3CDB222AAF6BCE476CCEEBFDF3FC136 (void);
+extern void PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[13] = 
+{
+	CameraController_Start_m8EAAC188934ECCBD009EC00751280DCFF4D96C71,
+	CameraController_Update_mBCB871B23DBA60444D43AB56D780478BA3D355E6,
+	CameraController__ctor_mE196A6332BDDED632D6F9DB6260E424594598950,
+	FlickeringLight_Start_m7D516566C903A2F68F7F1471820D1A9A029AB3A1,
+	FlickeringLight_Update_m155AEC6E38625104EEF180A44414F6FD7F46264F,
+	FlickeringLight__ctor_m05A8934D0DC3CC94EC5F4C71CEFFEE2F749759EF,
+	PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF,
+	PlayerController_Update_m1F4051EB5BCBCCE5EEE2E3E49B7E278C3B14EC33,
+	PlayerController_OnTriggerEnter_mF2704872AEA62333341DCBEA2831320C385701D1,
+	PlayerController_OnTriggerExit_m0A2DB7CCC3CDB222AAF6BCE476CCEEBFDF3FC136,
+	PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[13] = 
+{
+	1112,
+	1112,
+	1112,
+	1112,
+	1112,
+	1112,
+	1112,
+	1112,
+	940,
+	940,
+	1112,
+	2152,
+	1112,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	13,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
